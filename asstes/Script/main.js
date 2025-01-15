04ed2fb30901ecdf06322292a8d147057a50f95b
wedad osama <welkammash@gmail.com>
2025-01-15 23:00:54 +0300

    Add : Deta Section

--- asstes/Script/main.js
+++ asstes/Script/main.js
@@ -8,3 +8,36 @@ document.getElementById("toggle-theme").onclick = () =>{
 
   }
 };
+let userData = document.getElementById("user-deta"); 
+function getData() {
+  let data = ""; 
+  fetch(`https://api.github.com/users/wedadosama`)
+    .then((res) => res.json())
+    .then((data) => {
+      userData.innerHTML = `
+        <div class="avatar">
+          <img src="${data.avatar_url}" alt="${data.name}" />
+          <h1>${data.name}</h1>
+          <a hraf="${data.html_url}"target="_blank" > GitHub Profile</a>
+        </div>
+      `;
+    });
+    
+};
+fetch(`https://api.github.com/users/wedadosama/repos`)
+  .then((res) => res.json())
+  .then((data) => {
+    let repos = "";
+    for (let i = 0; i < data.length; i++) {
+      repos += `
+        <li>
+          <a href="${data[i].homepage}" target="_blank">${data[i].name}</a>
+        </li>
+      `;
+    }
+    document.getElementById("user-repos").innerHTML = repos;
+  });
+
+getData();
+
+
